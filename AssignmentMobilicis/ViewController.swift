077cd24fc643e672bddbf907e7d24d1a17ce2c19
Manvendu Pathak <manvendupathak2000@gmail.com>
2024-03-02 07:49:25 +0530

    FInal Commit

--- AssignmentMobilicis/ViewController.swift
+++ AssignmentMobilicis/ViewController.swift
@@ -4,16 +4,37 @@
 //
 //  Created by Manvendu Pathak on 01/03/24.
 //
-
+import SystemConfiguration
 import UIKit
+import NotificationCenter
+import AVFoundation
+import DeviceKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var deviceName: UILabel!
+    @IBOutlet weak var Model: UILabel!
+    @IBOutlet weak var Version: UILabel!
+    @IBOutlet weak var screenSize: UILabel!
+    @IBOutlet weak var processor: UILabel!
+    @IBOutlet weak var uniqueIdentifier: UILabel!
+    let device = Device.current
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(device.batteryState)
+        print(device.batteryLevel)
+        deviceName.text = UIDevice.current.model
+        Model.text = UIDevice.current.name
+        Version.text = "\(UIDevice.current.systemName)" + " \(UIDevice.current.systemVersion)"
+        processor.text = "\(device.cpu)"
+        screenSize.text = "\(device.diagonal)" + " inch"
+        uniqueIdentifier.text = "\(UIDevice.current.identifierForVendor!)"
+    
         // Do any additional setup after loading the view.
     }
-
-
 }
+
+
+
+
 
